1a87512cf5d9f04b141eab5b03192cb6ffa5b9ad
Denis Godoy <denisgodoy1@gmail.com>
2021-11-03 19:22:21 -0300

    update pipex

--- ft_check_args.c
+++ ft_check_args.c
@@ -6,7 +6,7 @@
 /*   By: degabrie <degabrie@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 15:42:45 by degabrie          #+#    #+#             */
-/*   Updated: 2021/11/03 18:51:59 by degabrie         ###   ########.fr       */
+/*   Updated: 2021/11/03 19:18:01 by degabrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	ft_check_args(t_pipex *pipex, int argc, char **argv)
 {
 	int		i;
 	int		j;
-	
+
 	if (argc < 5)
 	{
 		write(1, "Invalid number of arguments.\n", 29);
@@ -25,6 +25,8 @@ void	ft_check_args(t_pipex *pipex, int argc, char **argv)
 	pipex->infile = argv[1];
 	pipex->outfile = argv[argc - 1];
 	pipex->cmd = (char **)malloc((argc - 2) * sizeof(char *));
+	if (!pipex->cmd)
+		exit(EXIT_FAILURE);
 	i = 0;
 	j = 2;
 	while (j < argc - 1)
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: degabrie <degabrie@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 12:32:49 by degabrie          #+#    #+#             */
-/*   Updated: 2021/11/03 19:03:25 by degabrie         ###   ########.fr       */
+/*   Updated: 2021/11/03 19:21:39 by degabrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ int	main(int argc, char **argv)
 	}
 	close(fd1);
 	fd2 = open(pipex.outfile, O_CREAT | O_WRONLY | O_TRUNC, 0666);
-	write(fd2, pipex.cmd[0], ft_strlen(pipex.cmd[0]));
+	write(fd2, pipex.cmd[1], ft_strlen(pipex.cmd[1]));
 	close(fd2);
 	ft_free_arr(&pipex);
 	return (0);
