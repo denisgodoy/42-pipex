18156d4219afd8bf9a14ebaf824d954383320e09
Denis Godoy <denisgodoy1@gmail.com>
2021-11-17 19:00:44 -0300

    update pipex

--- ft_error_handler.c
+++ ft_error_handler.c
@@ -6,7 +6,7 @@
 /*   By: degabrie <degabrie@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/10 20:18:48 by degabrie          #+#    #+#             */
-/*   Updated: 2021/11/10 20:30:24 by degabrie         ###   ########.fr       */
+/*   Updated: 2021/11/17 18:48:00 by degabrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,10 @@
 
 void	ft_error_handler(int errno)
 {
-	perror(strerror(errno));
+	char	*err_msg;
+
+	err_msg = strerror(errno);
+	write(2, err_msg, ft_strlen(err_msg));
+	write(2, "\n", 1);
 	exit(EXIT_FAILURE);
 }
